9a6babcc205bb4f9a6707c8388b1b589cc724385
Michael Cahana <cahanamichael@gmail.com>
2019-01-25 14:52:48 -0600

    testing testthat when reading in excel file

--- test.R
+++ test.R
@@ -1,5 +1,15 @@
 library(testthat)
-source("function.R")
-expect_equal(add_nums(2, 2), 4)
-actual_size <- nrow(random_half_split(iris))
-expect_equal(actual_size, 75)
+library(tidyverse)
+library(readxl)
+
+
+# source("function.R")
+# expect_equal(add_nums(2, 2), 4)
+# actual_size <- nrow(random_half_split(iris))
+# expect_equal(actual_size, 75)
+
+test_data <- read_excel('read_in.xlsx')
+max <- test_data %>% filter(object=='max') %>% pull(value)
+expect_lte(max, 10)
+min <- test_data %>% filter(object=='min') %>% pull(value)
+expect_gte(min, 0)
